2aeccf1a55fcc77a4ba8b686b90c8a1b310a77d4
Alvaro Miranda Aguilera <kikitux@gmail.com>
2019-02-04 14:46:33 +0100

    Update vault.sh

--- vault-dev/vault.sh
+++ vault-dev/vault.sh
@@ -18,8 +18,10 @@ wget -O /tmp/vault.zip https://releases.hashicorp.com/vault/${VAULT}/vault_${VAU
 unzip -o -d /usr/local/bin /tmp/vault.zip
 
 mkdir -p /etc/vault.d
-curl -sL -O /etc/vault.d/server.hcl https://raw.githubusercontent.com/kikitux/curl-bash/master/vault-dev/vault.d/server.hcl
-curl -sL -O /etc/systemd/system/vault.service https://raw.githubusercontent.com/kikitux/curl-bash/master/vault-dev/vault.service
+#download server configuration
+curl -sL -o /etc/vault.d/server.hcl https://raw.githubusercontent.com/kikitux/curl-bash/master/vault-dev/vault.d/server.hcl
+#download systemd service
+curl -sL -o /etc/systemd/system/vault.service https://raw.githubusercontent.com/kikitux/curl-bash/master/vault-dev/vault.service
 
 systemctl enable vault.service
 systemctl start vault.service 2>/dev/null
