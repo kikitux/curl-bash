4c6290368cab673de1ca6e3afaf05fa27a7cf542
Alvaro Miranda Aguilera <kikitux@gmail.com>
2019-02-04 17:28:49 +0100

    add nomad-client

---
+++ nomad-client/nomad.d/client-dc1.hcl
@@ -0,0 +1,21 @@
+client {
+  enabled = true
+  server_join {
+    retry_join = [ "192.168.56.20" ]
+    retry_max = 5
+    retry_interval = "15s"
+  }
+  options = {
+    "driver.raw_exec" = "1"
+    "driver.raw_exec.enable" = "1"
+  }
+}
+
+bind_addr = "0.0.0.0"
+data_dir = "/var/lib/nomad"
+
+advertise {
+  # This should be the IP of THIS MACHINE and must be routable by every node
+  # in your cluster
+  rpc = "{{ GetInterfaceIP \"enp0s8\" }}"
+}
---
+++ nomad-client/nomad.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+which nomad &>/dev/null || {
+  apt-get update
+  apt-get install --no-install-recommends -y curl wget unzip jq
+
+  #always use highest release
+  NOMAD=$(curl -sL https://releases.hashicorp.com/nomad/index.json | jq -r '.versions[].version' | sort -V | egrep -v 'ent|beta|rc|alpha' | tail -n1)
+
+  # arch
+  if [[ "`uname -m`" =~ "arm" ]]; then
+    ARCH=arm
+  else
+    ARCH=amd64
+  fi
+
+  wget -O /tmp/nomad.zip https://releases.hashicorp.com/nomad/${NOMAD}/nomad_${NOMAD}_linux_${ARCH}.zip
+  unzip -d /usr/local/bin /tmp/nomad.zip
+}
+
+which docker java &>/dev/null || {
+apt-get update
+apt-get install --no-install-recommends -y docker.io
+apt-get install --no-install-recommends -y default-jre
+docker run hello-world &>/dev/null && echo docker hello-world works
+}
+
+# create dir and copy server.hcl for nomad
+mkdir -p /etc/nomad.d
+curl -o /etc/nomad.d/client.hcl https://raw.githubusercontent.com/kikitux/curl-bash/master/nomad-client/nomad.d/client-dc1.hcl
+curl -o /etc/systemd/system/nomad.service https://raw.githubusercontent.com/kikitux/curl-bash/master/nomad-1server/nomad.service
+
+# adjust interfce if not named enp0s8
+if [ "${IFACE}" ]; then
+  sed -i "s/enp0s8/${IFACE}/g" /etc/nomad.d/*.hcl
+fi
+
+# adjust retry-join
+if [ "${NOMAD_JOIN}" ] && [ -f /etc/nomad.d/client.hcl ]; then
+  list=$(printf '"%s"\n' "${NOMAD_JOIN//,/\",\"}")
+  sed -i "s/\"192.168.56.20\"/${list}/g" /etc/nomad.d/client.hcl
+fi
+
+systemctl enable nomad.service
+systemctl start nomad.service
