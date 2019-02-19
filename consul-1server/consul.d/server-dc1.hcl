56542bde15eb829279d2cf9d32a8dc324dd14e60
Alvaro Miranda Aguilera <kikitux@gmail.com>
2019-02-19 18:11:43 +0100

    enable scripts on consul

--- consul-1server/consul.d/server-dc1.hcl
+++ consul-1server/consul.d/server-dc1.hcl
@@ -5,5 +5,6 @@
   "client_addr": "0.0.0.0",
   "bind_addr": "{{ GetInterfaceIP \"enp0s8\" }}",
   "data_dir": "/usr/local/consul",
-  "bootstrap_expect": 1
+  "bootstrap_expect": 1,
+  "enable_local_script_checks": true
 }
--- provision/redis-server.sh
+++ provision/redis-server.sh
@@ -4,3 +4,24 @@ export DEBIAN_FRONTEND=noninteractive
 
 sudo apt-get update
 sudo apt-get install -y redis-server
+
+[ -d /etc/consul.d ] && {
+  cat <<EOF | sudo tee /etc/consul.d/redis.json
+{
+  "service": {
+    "name": "redis",
+    "port": 6379,
+    "address": "127.0.0.1"
+  },
+  "checks": [
+          {
+            "name": "redis-basic-connectivity",
+            "args": ["/usr/bin/redis-cli", "-h", "127.0.0.1", "-p", "6379", "ping"],
+            "interval": "10s"
+          }
+        ]
+}
+EOF
+
+  sudo service consul reload
+}
