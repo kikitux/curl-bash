9271215367edf471a034f5a2387c115d46aee0d7
Alvaro Miranda Aguilera <kikitux@gmail.com>
2019-02-24 16:55:03 +0100

    update

--- consul-client/consul.sh
+++ consul-client/consul.sh
@@ -6,6 +6,18 @@ which consul &>/dev/null || {
   apt-get update
   apt-get install --no-install-recommends -y curl wget unzip dnsmasq jq
 
+  CONSUL=$(curl -sL https://releases.hashicorp.com/consul/index.json | jq -r '.versions[].version' | sort -V | egrep -v 'ent|beta|rc|alpha' | tail -n1)
+
+  # arch
+  if [[ "`uname -m`" =~ "arm" ]]; then
+    ARCH=arm
+  else
+    ARCH=amd64
+  fi
+
+  wget -q -O /tmp/consul.zip https://releases.hashicorp.com/consul/${CONSUL}/consul_${CONSUL}_linux_${ARCH}.zip
+  unzip -o -d /usr/local/bin /tmp/consul.zip
+
   # dnsmasq to use consul dns
   tee /etc/dnsmasq.d/10-consul <<EOF
 server=/consul/127.0.0.1#8600
@@ -21,17 +33,6 @@ EOF
     killall -HUP systemd-resolved
   }
 
-  CONSUL=$(curl -sL https://releases.hashicorp.com/consul/index.json | jq -r '.versions[].version' | sort -V | egrep -v 'ent|beta|rc|alpha' | tail -n1)
-
-  # arch
-  if [[ "`uname -m`" =~ "arm" ]]; then
-    ARCH=arm
-  else
-    ARCH=amd64
-  fi
-
-  wget -q -O /tmp/consul.zip https://releases.hashicorp.com/consul/${CONSUL}/consul_${CONSUL}_linux_${ARCH}.zip
-  unzip -o -d /usr/local/bin /tmp/consul.zip
 }
 
 # create dir and copy server.hcl for consul
