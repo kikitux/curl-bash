0c9ba26cb37d8e297a9290e63739723353b4f404
Alvaro Miranda Aguilera <kikitux@gmail.com>
2019-03-23 10:45:00 +0100

    update

---
+++ provision/download_product.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+[ "${PRODUCT}" ] || {
+	echo warn: script need variable PRODUCT=name_of_product
+	echo info: skipping run as no product was given
+	exit 0
+}
+
+which ${PRODUCT} &>/dev/null || {
+
+  # download tools
+  which curl wget unzip jq || {
+    export DEBIAN_FRONTEND=noninteractive
+    apt-get update
+    apt-get install --no-install-recommends -y curl wget unzip jq
+  }
+
+  VERSION=$(curl -sL https://releases.hashicorp.com/${PRODUCT}/index.json | jq -r '.versions[].version' | sort -V | egrep -v 'ent|beta|rc|alpha' | tail -n1)
+
+  # arch
+  if [[ "`uname -m`" =~ "arm" ]]; then
+    ARCH=arm
+  else
+    ARCH=amd64
+  fi
+
+  wget -q -O /tmp/${PRODUCT}.zip https://releases.hashicorp.com/${PRODUCT}/${VERSION}/${PRODUCT}_${VERSION}_linux_${ARCH}.zip
+  unzip -o -d /usr/local/bin /tmp/${PRODUCT}.zip
+
+}
