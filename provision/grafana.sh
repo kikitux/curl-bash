0d250c73b1a04fdde3681ee6a19663fbf702b753
Alvaro Miranda Aguilera <kikitux@gmail.com>
2019-03-23 18:01:43 +0100

    update

--- provision/grafana.sh
+++ provision/grafana.sh
@@ -8,6 +8,7 @@ which grafana-server &>/dev/null || {
   apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8C8C34C524098CB6
   echo 'deb https://packages.grafana.com/oss/deb stable main' | tee /etc/apt/sources.list.d/grafana.list
   curl -fsSL https://packages.grafana.com/gpg.key | apt-key add -
+  apt-get update
   apt-get install -y grafana apt-transport-https
   systemctl daemon-reload
   systemctl enable grafana-server
