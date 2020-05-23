da099dc65aaab4b33768fe6b2bd55f99f8b6a118
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2020-05-23 15:14:09 +0200

    Update go.sh

--- provision/go.sh
+++ provision/go.sh
@@ -1,31 +1,34 @@
 #!/usr/bin/env bash
 
-# make sure apt database is up-to date
-apt-get update
+which go || {
+  # make sure apt database is up-to date
+  apt-get update
 
-# install golang-${GOVER}
-apt-get install -y snapd
-snap install go --classic
+  # install golang-${GOVER}
+  apt-get install -y snapd
+  snap install go --classic
 
-# set base
-# if we are in sudo use the calling user
-# we use eval as ~ won't be expanded
-if [ "${SUDO_USER}" ]; then
-  BASE="`eval echo ~${SUDO_USER}/.bash_profile`"
-else
-  BASE="`eval echo ~/.bash_profile`"
-fi
+  # set base
+  # if we are in sudo use the calling user
+  # we use eval as ~ won't be expanded
+  if [ "${SUDO_USER}" ]; then
+    BASE="`eval echo ~${SUDO_USER}/.bash_profile`"
+  else
+    BASE="`eval echo ~/.bash_profile`"
+  fi
 
-grep 'GOPATH|GOROOT' ${BASE} &>/dev/null || {
-  sudo mkdir -p ~/go
-  [ -f ${BASE} ] && cp ${BASE} ${BASE}.ori
-  grep -v 'GOPATH|GOROOT' ${BASE}.ori | sudo tee -a ${BASE}
-  echo 'export GOROOT=/snap/go/current' | sudo tee -a ${BASE}
-  echo 'export PATH=$PATH:/snap/bin:$GOROOT/bin' | sudo tee -a ${BASE}
-  echo 'export GOPATH=~/go' | sudo tee -a ${BASE}
+  grep 'GOPATH|GOROOT' ${BASE} &>/dev/null || {
+    sudo mkdir -p ~/go
+    [ -f ${BASE} ] && cp ${BASE} ${BASE}.ori
+    grep -v 'GOPATH|GOROOT' ${BASE}.ori | sudo tee -a ${BASE}
+    echo 'export GOROOT=/snap/go/current' | sudo tee -a ${BASE}
+    echo 'export PATH=$PATH:/snap/bin:$GOROOT/bin' | sudo tee -a ${BASE}
+    echo 'export GOPATH=~/go' | sudo tee -a ${BASE}
+  }
+
+  if [ "${SUDO_USER}" ]; then
+    chown ${SUDO_USER} ${BASE}
+    [ -f ${BASE}.ori ] && chown ${SUDO_USER} ${BASE}.ori
+  fi
+
 }
-
-if [ "${SUDO_USER}" ]; then
-  chown ${SUDO_USER} ${BASE}
-  [ -f ${BASE}.ori ] && chown ${SUDO_USER} ${BASE}.ori
-fi
