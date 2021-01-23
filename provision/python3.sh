5f20372b2aa2af102d30d6cce41eb2a1844184f7
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2021-01-23 12:59:09 +0100

    Update python3.sh

--- provision/python3.sh
+++ provision/python3.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
+export DEBIAN_FRONTEND=noninteractive
+  
+which python3 &>/dev/null || {
+  apt-get update
+  apt-get install -y python3
+}
+
 which pip3 &>/dev/null || {
-  export DEBIAN_FRONTEND=noninteractive
-
-  which python3 &>/dev/null || {
-    apt-get update
-    apt-get install -y python3
-  }
-
   apt-get install -y python3-pip
+}
 
+which pipenv &>/dev/null || {
+  pip3 install pipenv
 }
